e2f81e568ec9c537b57d107436d3f829c57635e5
pattonliu84 <113130211+pattonliu84@users.noreply.github.com>
2023-02-02 11:12:45 -0800

    Update Main.java

--- src/main/java/u5pp/Main.java
+++ src/main/java/u5pp/Main.java
@@ -5,10 +5,7 @@ import java.util.Scanner;
 public class Main {
     
     public static void main(String[] args) {
-        System.out.println("Hello! :)\n" + 
-        "Welcome to the unit 5 programming project!\n"+
-        "Take a look at the README, and the txt files to start\n" + 
-        "Good luck!");
-
+        Scanner scanner = new Scanner(System.in);
+        Chess.play(scanner);
     }
 }
